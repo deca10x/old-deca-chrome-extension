6acfbf7307abda472836e3865e48d9480ed53e63
Graham Tremper <graham@tremper.xyz>
2021-06-22 16:58:08 -0700

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  importOrder: ["^(v3|components|lib|styles)/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+};
