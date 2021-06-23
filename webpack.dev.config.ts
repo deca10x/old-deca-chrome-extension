9936d22baec1cebd702a4922acbeb5ed5d579f24
Graham Tremper <graham@tremper.xyz>
2021-06-22 19:46:02 -0700

    Create common webpack config

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,53 +1,17 @@
 import path from 'path';
 import webpack from 'webpack';
 import webpackDevServer from 'webpack-dev-server';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import ESLintPlugin from 'eslint-webpack-plugin';
+import { merge } from 'webpack-merge';
+
+import { commonConfig } from './webpack.config';
 
 interface Configuration extends webpack.Configuration {
   devServer?: webpackDevServer.Configuration;
 }
 
-const config: Configuration = {
+const config = merge<Configuration>(commonConfig, {
   mode: 'development',
-  output: {
-    publicPath: '/',
-  },
-  entry: './src/popup.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/popup.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-    }),
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'build'),
@@ -56,6 +20,6 @@ const config: Configuration = {
     open: true,
     hot: true,
   },
-};
+});
 
 export default config;
--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -1,66 +1,18 @@
-import path from 'path';
 import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import ESLintPlugin from 'eslint-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { merge } from 'webpack-merge';
 
-const config: webpack.Configuration = {
+import { commonConfig } from './webpack.config';
+
+const config = merge<webpack.Configuration>(commonConfig, {
   mode: 'production',
-  entry: {
-    popup: './src/popup.tsx',
-    background: './src/background.ts',
-    options: './src/options.tsx',
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-    publicPath: '',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/popup.html',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/options.html',
-      filename: 'options.html',
-      chunks: ['options'],
-    }),
     new CopyPlugin({
       patterns: ['manifest.json', { from: 'images', to: 'images' }],
     }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-    }),
     new CleanWebpackPlugin(),
   ],
-};
+});
 
 export default config;
