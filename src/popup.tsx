759972d61ab1937292c0ba976468f06436834b37
Graham Tremper <graham@tremper.xyz>
2021-06-22 17:23:02 -0700

    Add types for chrome

--- src/popup.tsx
+++ src/popup.tsx
@@ -9,3 +9,34 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Initialize button with user's preferred color
+const changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', ({ color }) => {
+  if (changeColor) {
+    changeColor.style.backgroundColor = color;
+  }
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+if (changeColor) {
+  changeColor.addEventListener('click', async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab.id) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+      });
+    }
+  });
+}
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
