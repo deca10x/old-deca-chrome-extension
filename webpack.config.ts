908b4dfafb156511d842878910b891d785a9c094
Graham Tremper <graham@tremper.xyz>
2021-06-30 21:37:30 -0700

    Remove unused

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,6 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 export const commonConfig: webpack.Configuration = {
   entry: {
-    // popup: './src/popup.tsx',
     background: './src/background.ts',
     options: './src/options.tsx',
   },
@@ -34,11 +33,6 @@ export const commonConfig: webpack.Configuration = {
     extensions: ['.tsx', '.ts', 'jsx', '.js'],
   },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   template: 'src/popup.html',
-    //   filename: 'popup.html',
-    //   chunks: ['popup'],
-    // }),
     new HtmlWebpackPlugin({
       template: 'src/options.html',
       filename: 'options.html',
